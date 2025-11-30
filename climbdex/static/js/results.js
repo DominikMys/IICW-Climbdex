fcb7f5eff24eb1f0de32e5c0acb70f2c1dff7452
Dominik <d.mysliwiec@student.utwente.nl>
2025-11-30 14:37:48 +0100

    fix api

--- climbdex/static/js/results.js
+++ climbdex/static/js/results.js
@@ -347,7 +347,7 @@ async function fetchResults(pageNumber, pageSize) {
 async function fetchGenerateResults() {
     const loading = document.getElementById("loading");
     loading.style.display = "block";
-    const response = await fetch("http://127.0.0.1:5000/generate", {
+    const response = await fetch("/api/generate", {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
